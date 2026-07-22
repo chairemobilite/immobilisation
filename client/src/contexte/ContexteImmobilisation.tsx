2948f6e5219cfdfb8d657e6471a12819d276121b
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-07-22 09:53:20 -0400

    Added theme to application
    In the past I've always had a bunch of custom styles which I've had to
    add to buttons. Implementing theme in order to minimize the amount of
    boiler plate stuff I have to introduce in the frontend coding of MUI
    buttons. Current implementation doesn't allow to toggle theme
    as it would require cleaning up all the boiler plate color definitions
    in the app so for now only the regulation set page gets an update
    
    Did this now because I'm implementing the copy reg set function which requires
    page to use MUI components and I want to reduce the amount of boilerplate
    I introduce.
    
    Changed default color to a light grey for the buttons otherwise it was
    blue on black which created some visibility issues
    
    Changed common css to remove all the custom colors and rely on MUI theme.
    This might create some issues in the short term of unusual buttons showing
    up as grey but long term goal is to pull all the app to this setup
    
    Added comments to context to make it clearer what things do

--- client/src/contexte/ContexteImmobilisation.tsx
+++ client/src/contexte/ContexteImmobilisation.tsx
@@ -1,13 +1,34 @@
+/*
+Copyright (c) 2026 Paul Charbonneau
+
+Licensed under the MIT License.
+See the LICENSE file in the project root for license information.
+
+A context tool so that all the pages can consume data and that user interface 
+choices are persistent
+*/
+
+
 import { createContext, useState, ReactNode, Dispatch, SetStateAction, useContext } from 'react';
-import { CentreDeCarte, ContexteImmobilisationType, donneesCarteDeFond, FournisseurContexteProps } from '../types/ContextTypes';
+import { CentreDeCarte, ContexteImmobilisationType, donneesCarteDeFond, FournisseurContexteProps,ThemeMode } from '../types/ContextTypes';
 import { latLng } from 'leaflet';
+    import { createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useMemo } from 'react';
+import { ThemeProvider } from '@mui/system';
 
 
 const ContexteImmobilisation = createContext<ContexteImmobilisationType | undefined>(undefined);
 
 
-
+/**
+ * this provides mapping and theme related items which could be used through out the app
+ * @param children the components which consume the context
+ * @returns a context provider which is used to to bracket other components and a 
+ * useContext function which allows unpacking the context
+ */
 const FournisseurContexte = ({ children }: FournisseurContexteProps) => {
+    // Mapping options for tiles
     const cartoPossibles: donneesCarteDeFond[] = [
         {
             id: 0,
@@ -31,24 +52,41 @@ const FournisseurContexte = ({ children }: FournisseurContexteProps) => {
             zoomMax:19
         }
     ];
+    // the mapping options state. No sure this really needs to be a state honestly
     const [optionsCartos, defOptionsCarto] = useState<donneesCarteDeFond[]>(cartoPossibles);
     const [optionCartoChoisie, defOptionCartoChoisie] = useState<number>(0);
-
+    // the changer for the mapping option
     const changerCarto = (idAUtiliser: number) => {
         defOptionCartoChoisie(idAUtiliser)
     }
+    // basic indication of which theme to use
+    const [mode, setMode] = useState<ThemeMode>('dark');
+
+    const theme = useMemo(
+        () => mode==='dark'?createTheme({ palette: { mode, primary: { main: '#cccbd3' } } }):createTheme({ palette: { mode, } }),
+        [mode]
+    );
+
+    const toggleTheme = () => setMode(m => (m === 'light' ? 'dark' : 'light'));
 
 
     return (
+        <ThemeProvider theme={theme}>
+      <CssBaseline />
         <ContexteImmobilisation.Provider 
             value={{ 
                 optionCartoChoisie, 
                 changerCarto, 
                 optionsCartos,
+                mode,
+                toggleTheme,
+                setTheme:setMode
              }}
         >
             {children}
         </ContexteImmobilisation.Provider>
+
+            </ThemeProvider>
     );
 };
 const utiliserContexte = () => {
