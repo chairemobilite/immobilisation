0930611bf744bb13b7f81e61562f89bad6b1412f
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-07-25 00:46:27 -0400

    Changed framework from create react app to vite
    Create react app is deprecated and no longer supported. I'm proposing
    we move this to a more modern framework. To do so,I've
    
    - changed the docker compose to change the name of the environment
    variable to VITE_ rathern than REACT_APP so that it can be read by the
    app.
    
    - changed api.ts in order for it to use the vite import.meta.env means
    of importing environment variables.
    
    - changed the docker file to run npm run dev rather than react scripts
    
    - added the eslint config and vite config and moved the index.html out
    of public and into the main source directory. I've also changed it
    so it refers to the index.tsx whixh is required
    
    - I've added removed react-scripts and added vite and vite react plugin
    
    - change tsconfig to stop having deprecation message on the module
    resolution and cleaned up the typeroots. Hasn't affected anything. seems
    like an artefact of my early vibe coding efforts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
 const api = axios.create({
-  baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
